0a0d4f759ede02c198d5591ad66cce9b7aa04fc2
PauliusTaraskevicius <fixas1029@gmail.com>
2024-08-20 08:54:56 +0300

    made jobs and pagination client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,14 @@
-"use client";
-import { useState } from "react";
-
 import { Banner } from "@/components/banner";
-import { Jobs } from "@/components/jobs/jobs";
 import { PaginationSection } from "@/components/pagination-section";
 
 import { DUMMY_DATA } from "@/DUMMY_DATA";
-import Link from "next/link";
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(20);
-
-  const data = DUMMY_DATA;
-
-  const lastItemIndex = currentPage * itemsPerPage;
-  const firstItemIndex = lastItemIndex - itemsPerPage;
-  const currentItems = data.slice(firstItemIndex, lastItemIndex);
-
   return (
     <div className="flex flex-col justify-center items-center h-full">
       <Banner />
-      {currentItems.map((job) => (
-        <div key={job.title} className="w-full py-1.5 cursor-pointer">
-          <Link href={`/job/${job.id}`}>
-            <Jobs job={job} />
-          </Link>
-        </div>
-      ))}
-      <PaginationSection
-        totalItems={data.length}
-        itemsPerPage={itemsPerPage}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+
+      <PaginationSection jobsData={DUMMY_DATA} />
     </div>
   );
 }
--- components/pagination-section.tsx
+++ components/pagination-section.tsx
@@ -1,4 +1,7 @@
+"use client";
 
+import { useState } from "react";
+import Link from "next/link";
 
 import {
   Pagination,
@@ -10,19 +13,24 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 
+import { Job } from "@/lib/types";
+import { Jobs } from "./jobs/jobs";
+
 type Props = {
-  totalItems: number;
-  itemsPerPage: number;
-  currentPage: number;
-  setCurrentPage: (items: number) => void;
+  jobsData: Job[];
 };
 
-export const PaginationSection = ({
-  currentPage,
-  itemsPerPage,
-  setCurrentPage,
-  totalItems,
-}: Props) => {
+export const PaginationSection = ({ jobsData }: Props) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(20);
+
+  const data = jobsData;
+
+  const lastItemIndex = currentPage * itemsPerPage;
+  const firstItemIndex = lastItemIndex - itemsPerPage;
+  const currentItems = data.slice(firstItemIndex, lastItemIndex);
+  const totalItems = data.length;
+
   let pages = [];
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
     pages.push(i);
@@ -41,37 +49,48 @@ export const PaginationSection = ({
   };
 
   return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            className="cursor-pointer"
-            onClick={() => handlePrevPage()}
-          />
-        </PaginationItem>
-        {pages.map((page, i) => (
-          <PaginationItem
-            key={i}
-            className={currentPage === page ? "bg-neutral-200 rounded-md" : ""}
-          >
-            <PaginationLink
+    <>
+      {currentItems.map((job) => (
+        <div key={job.title} className="w-full py-1.5 cursor-pointer">
+          <Link href={`/job/${job.id}`}>
+            <Jobs job={job} />
+          </Link>
+        </div>
+      ))}
+      <Pagination className="py-6">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
               className="cursor-pointer"
-              onClick={() => setCurrentPage(page)}
-            >
-              {page}
-            </PaginationLink>
+              onClick={() => handlePrevPage()}
+            />
           </PaginationItem>
-        ))}
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext
-            className="cursor-pointer"
-            onClick={() => handleNextPage()}
-          />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+          {pages.map((page, i) => (
+            <PaginationItem
+              key={i}
+              className={
+                currentPage === page ? "bg-neutral-200 rounded-md" : ""
+              }
+            >
+              <PaginationLink
+                className="cursor-pointer"
+                onClick={() => setCurrentPage(page)}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              className="cursor-pointer"
+              onClick={() => handleNextPage()}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </>
   );
 };
